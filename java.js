41f59fc587a80bbff86fb52b4715d8d7cd081357
Prithviraj03 <71976945+Prithviraj03@users.noreply.github.com>
2021-04-07 02:31:04 +0530

    Update java.js

--- java.js
+++ java.js
@@ -16,15 +16,15 @@ function Options(){
   var A3=document.getElementById("A3");
 
 if (A3.checked || A2.checked || A1.checked) {
-  if (A3.checked) {
-    alert("Other is checked ");
+ Else if (A3.checked) {
+   alert("Other is checked ");
   var B3=document.getElementById("B3").innerHTML="Other";
 }
-  if  (A2.checked) {
+ Else if  (A2.checked) {
     alert("Company is checked ");
   var B2=document.getElementById("B2").innerHTML="Company";
 }
-  if  (A1.checked) {
+ Else if  (A1.checked) {
     alert("Blog is checked ");
   var B1=document.getElementById("B1").innerHTML="Blog";
    }
